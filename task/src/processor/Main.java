760b37181904eca8d4af16b0d511d449a973d6f3
Jeremy Grecu <jeremy.grecu@sap.com>
2021-08-14 11:08:36 +0100

    JBA - Numeric Matrix Processor - create the getMatrix & printMatrix methods

--- task/src/processor/Main.java
+++ task/src/processor/Main.java
@@ -1,7 +1,32 @@
 package processor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final Scanner scanner = new Scanner(System.in);
+        int[][] firstMatrix = getMatrix(scanner);
+        printMatrix(firstMatrix);
+    }
+
+    private static int[][] getMatrix(Scanner scanner) {
+        int rows1 = scanner.nextInt();
+        int columns1 = scanner.nextInt();
+        int[][] matrix = new int[rows1][columns1];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns1; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
